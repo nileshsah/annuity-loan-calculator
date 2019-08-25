66f273544b966946b68054093e47511c60500371
Nilesh Sah <nilesh.sah13@gmail.com>
2019-08-25 20:30:38 +0200

    Init project with spring initalizer

---
+++ src/test/java/com/finance/loancalculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finance.loancalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoanCalculatorApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
