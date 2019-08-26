b192b2df62291687b0407ce6ea18d79364029f45
Nilesh Sah <nilesh.sah13@gmail.com>
2019-08-26 22:23:44 +0200

    Add custom message for NotNull javax request validation

--- src/main/java/com/finance/loancalculator/dto/AnnuityLoanRequest.java
+++ src/main/java/com/finance/loancalculator/dto/AnnuityLoanRequest.java
@@ -16,18 +16,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class AnnuityLoanRequest {
 
-  @NotNull
-  @DecimalMin(value = "0.01", message = "The loan amount must be at least 0.01")
+  @NotNull(message = "Loan Amount must be specified")
+  @DecimalMin(value = "100", message = "The loan amount must be at least 100")
   private Double loanAmount;
 
-  @NotNull
+  @NotNull(message = "Nominal Rate must be specified")
   @DecimalMin(value = "0", message = "The nominal rate must be >= 0%")
   private Double nominalRate;
 
-  @NotNull
+  @NotNull(message = "Loan duration must be specified")
   @Min(value = 1, message = "The duration for the loan must be greater than 0")
   private Integer duration;
 
-  @NotNull
+  @NotNull(message = "Start date for the loan is required")
   private LocalDateTime startDate;
 }
